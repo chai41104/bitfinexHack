74c03d51f486e9ee6ac2771fddadcc6dccff98d4
Thongchai <chai41104@hotmail.com>
2018-07-21 20:29:31 +0100

    Add static website

--- express/server.js
+++ express/server.js
@@ -6,7 +6,6 @@ var express = require('express');
 var app = express();
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
-var Component = require('./MainComponent.js');
 var path = require('path');
 
 const BFX = require('bitfinex-api-node')
@@ -42,7 +41,6 @@ ws.on('open', () => {
   {
 	  //ws.subscribeTicker('fUSD')
 	  let name = 't'+ symbol_list[i].toUpperCase()
-	  console.log(name)
 	  ws.subscribeTicker(name)
   }
 	// ws.subscribeTicker('fUSD')
@@ -55,17 +53,16 @@ ws.on('open', () => {
 // })
 
 ws.onTicker({}, (ticker) => {
-  console.log( ticker.toJS())
   data.push(ticker.toJS())
 })
 
 ws.open()
 
 // app.use(express.static(path.join(__dirname, './www')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', function(request, response){
 	response.setHeader('Access-Control-Allow-Origin', '*')
-	console.log('i was here')
 	var res = []
 	// let end = Math.min(data.length, start+10)
 	let end = data.length
@@ -78,10 +75,7 @@ app.get('/', function(request, response){
 	start = end;
 });
 
-
 var PORT=3000;
 app.listen(PORT, function(){
 	console.log('http://localhost:' + PORT);
 });
-	
-	
