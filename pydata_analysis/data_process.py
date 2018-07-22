cc7e38bacb65beaba9417fb4408d6e42def381e5
motatoes <moe.habib9@gmail.com>
2018-07-22 09:47:59 +0100

    david

---
+++ pydata_analysis/data_process.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# CC SH 18
+#------------------------------------------------------------------------------
+
+import ccxt
+import numpy as np
+import time
+import pandas as pd
+#import matplotlib.pyplot as plt
+#from scipy.stats import norm
+import json
+from tqdm import tqdm
+bitfinex = ccxt.bitfinex({'enableRateLimit': True})
+bitfinex.rateLimit = bitfinex.rateLimit * 3
+bitfinex.load_markets()
+ethfinex = ccxt.ethfinex({'enableRateLimit': True})
+ethfinex.load_markets()
+
+#------------------------------------------------------------------------------
+# Class definitions
+
+def caclulate_median_percent_change(exchange, ccxt_pair):
+    candles = exchange.fetch_ohlcv(ccxt_pair, timeframe='1d', since=timestamp_start * 1000)
+    df_candles = pd.DataFrame(candles)
+    volumes = df_candles[5]
+    volumes.index = pd.to_datetime(df_candles[0], unit='ms')
+    #volumes.plot()
+    volumes_filtered = volumes.rolling(window=4, center=False).median()
+    #volumes_filtered.plot()
+    a = volumes_filtered.pct_change().abs()
+    #percent_abnormal = a[a >= percent_change_thres].size / a.size
+    return a.median()
+
+def get_active_symbols(exchange):
+    return [symbol for symbol in exchange.symbols if is_active_symbol(exchange, symbol)]
+
+def is_active_symbol(exchange, symbol):
+    return ('.' not in symbol) and (('active' not in exchange.markets[symbol]) or (exchange.markets[symbol]['active']))
+
+
+# Poll the 24 Hour volume data
+
+# Params
+
+days_back = 100
+percent_change_thres = 0.4
+
+timestamp_start = int(np.floor(time.time())) - days_back * 24 * 60 * 60
+
+# main
+if __name__ == '__main__':
+    data_json = dict()
+    for symbol in tqdm(get_active_symbols(bitfinex)):
+        print('[INFO] Fetching symbol: {0}'.format(symbol))
+        median_percent_change = caclulate_median_percent_change(bitfinex, symbol)
+        tmp = dict()
+        tmp['medianpc'] = median_percent_change
+        data_json[symbol] = tmp
+        with open('MedianPercentChange.json', 'w') as outfile:
+            json.dump(data_json, outfile)
+        
