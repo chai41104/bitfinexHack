1fa15de9720a32288b183bd7e26503cdda53530a
motatoes <moe.habib9@gmail.com>
2018-07-22 09:47:47 +0100

    server

--- express/server.js
+++ express/server.js
@@ -55,6 +55,8 @@ ws.open()
 
 // app.use(express.static(path.join(__dirname, './www')));
 app.use('/theme', express.static(path.join(__dirname, '../public')));
+app.use('/json', express.static(path.join(__dirname, './json')));
+app.use('/chart', express.static(path.join(__dirname, 'chart.html')));
 
 app.get('/', function(request, response){
 	response.setHeader('Access-Control-Allow-Origin', '*')
